0133b6324c7cfb3763377c2425bbcfd12b7f206d
ThisIsMiha <mihaela22641@gmail.com>
2016-03-08 17:33:43 +0100

    Procistili cmo PHP.

--- index.php
+++ index.php
@@ -1,28 +1,12 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Ovo piše u kartici</title>
-	</head>
+<head>
+	<meta charset="UTF-8">
+	<title>Forme</title>
+</head>
+<body>
+	<h1>Forme</h1>
 
-	<body>
-		<h1>Naslov 1</h1>
-		<p>Čemu služi p? To je odlomak.</p>
-		<!-- komentar u html -->
-		<?php
-		
-		//komentar
-		echo('nešto' .date('d.m.Y'));
-		
-		?>
-
-		<?php
-		
-		$broj = 12;
-		echo($broj);
-		
-		?>
-
-	</body>
+</body>
 </html>
 
