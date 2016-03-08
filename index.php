95e068467d5c678b91f318d04f79041febaee338
ThisIsMiha <mihaela22641@gmail.com>
2016-03-08 17:16:51 +0100

    Prvi commit forma. Sretan dan zena.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ovo piše u kartici</title>
+	</head>
+
+	<body>
+		<h1>Naslov 1</h1>
+		<p>Čemu služi p? To je odlomak.</p>
+		<!-- komentar u html -->
+		<?php
+		
+		//komentar
+		echo('nešto' .date('d.m.Y'));
+		
+		?>
+
+		<?php
+		
+		$broj = 12;
+		echo($broj);
+		
+		?>
+
+	</body>
+</html>
+
